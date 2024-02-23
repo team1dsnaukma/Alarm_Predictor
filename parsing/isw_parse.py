2555204da96d3d5d0411c48421b59dee0b15b480
zorian <devgen958@gmail.com>
2024-02-23 02:07:30 +0200

    pages request through 1-year period; inappropriate data cleaning (>80%); csv writer

--- parsing/isw_parse.py
+++ parsing/isw_parse.py
@@ -1,27 +1,44 @@
-import datetime
+import csv
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 
-# Get html data from ISW via requests
-base_url = "https://www.understandingwar.org/" \
-           "backgrounder/russian-offensive-campaign-assessment"
-# Set time period from 2022-02-24 to 2023-01-25
-period = datetime.datetime(2023, 1, 25) - datetime.datetime(2022, 2, 24)
-print(period)
-# page = requests.get(base_url + "-february-19-2024")
-# html_data = BeautifulSoup(page.content, "html.parser")
-# # <p> tag elements
-# pte = html_data.find_all("p")
-# # Get valuable info
-# parsed_data = []
-# for elem in pte:
-#     if not elem.find_all(True):
-#         parsed_data.append(elem)
-# # Data clearing
-# for datum in parsed_data[::-1]:
-#     if datum.text == "Nothing significant to report.":
-#         parsed_data.remove(datum)
-#
-# for datum in parsed_data:
-#     print(datum)
-#
+BASE = "https://www.understandingwar.org/" \
+       "backgrounder/russian-offensive-campaign-assessment"
+
+start_date = datetime(2022, 2, 24)
+end_date = datetime(2023, 1, 25)
+period = end_date - start_date
+
+
+def get_news_by_page(data):
+    parsed_page = []
+    # ignore first three <p> tags about report info
+    for elem in data.find_all("p")[3:]:
+        if not elem.find_all("a"):
+            parsed_page.append(elem.text)
+    # cleaning bad data
+    for row in parsed_page[::-1]:
+        if row.startswith(("[", "\xa0")):
+            parsed_page.remove(row)
+        else:
+            break
+    if parsed_page and parsed_page[-1] == "Immediate items to watch":
+        parsed_page.pop()
+    return "\n".join(parsed_page)
+
+
+with open("../raw_data_from_parsing/isw/isw.csv", 'w', encoding="utf-8") as f:
+    writer = csv.writer(f)
+    for delta in range(period.days):
+        date = start_date + timedelta(delta)
+        page = BeautifulSoup(
+            requests.get(BASE + date.strftime("-%B-%d-%Y")).content,
+            "html.parser"
+        )
+        if page.find("link")["href"] == "/404":
+            page = BeautifulSoup(
+                requests.get(BASE + date.strftime("-%B-%d")).content,
+                "html.parser"
+            )
+        writer.writerow([date.strftime("%d-%m-%Y"), get_news_by_page(page)])
